d6484a763437527d01131fd812c7063c830828fb
Shoukath <shoukathali271004@gmail.com>
2024-03-02 00:50:27 +0530

    updated the website so that the no button goes to different, random locations when pressed

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 document.getElementById("noButton").addEventListener("click", function () {
+  // Get the button element
+  let noButton = document.getElementById("noButton");
+
+  // Generate random position values
+  let randomX = Math.random() * (window.innerWidth - noButton.offsetWidth);
+  let randomY = Math.random() * (window.innerHeight - noButton.offsetHeight);
+
+  // Set the new position
+  noButton.style.position = "absolute";
+  noButton.style.left = randomX + "px";
+  noButton.style.top = randomY + "px";
+});
+
+document.getElementById("noButton").addEventListener("click", function () {
   // Increase the font size of the "Yes" button
   let yesButton = document.getElementById("yesButton");
   let currentFontSize = parseInt(window.getComputedStyle(yesButton).fontSize);
